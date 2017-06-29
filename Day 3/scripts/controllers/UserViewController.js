a8e54a4344c7d57c33b315c9f5f97ea210371493
laura.petre <laura.p1012@gmail.com>
2017-06-29 16:44:46 +0300

    Frontend day3: Angular Laura Petre, Robert Iancu

--- Day 3/scripts/controllers/UserViewController.js
+++ Day 3/scripts/controllers/UserViewController.js
@@ -1,8 +1,11 @@
 /**
  * Created by Laura.Petre on 6/29/2017.
  */
-hrApp.controller('UserViewController', ['$scope', '$http', '$routeParams', '$location',
-    function($scope, $http, $routeParams, $location) {
+hrApp.controller('UserViewController', ['$scope', '$http', '$routeParams', '$location','UserList',
+    function($scope, $http, $routeParams, $location,UserList) {
+
+
+
         var User= function(firstname,lastname,email,phone,CNP,profession){
             this.firstname=firstname;
             this.lastname=lastname;
@@ -13,7 +16,7 @@ hrApp.controller('UserViewController', ['$scope', '$http', '$routeParams', '$loc
         }
 
         $scope.user = new User("","","","","","");
-        $scope.userList=[];
+        $scope.userList=UserList;
         $scope.listShow=true;
 
         $scope.back = function() {
@@ -23,8 +26,10 @@ hrApp.controller('UserViewController', ['$scope', '$http', '$routeParams', '$loc
             $scope.user=new User("","","","","","");
         }
         $scope.save = function() {
-            $scope.userList.push($scope.user);
+            UserList.push($scope.user);
+      //      $scope.userList.push($scope.user);
             alert("User was added: "+$scope.user.firstname+$scope.user.lastname);
+            $scope.user=new User("","","","","","");
         }
         $scope.toggleShowList = function() {
             $scope.listShow=! ($scope.listShow);
---
+++ Day 3/scripts/services/demo/UserService.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Laura.Petre on 6/29/2017.
+ */
+
+hrApp.value('UserList', []);
